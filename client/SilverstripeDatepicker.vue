cef97e89bc429e4437e71144ce229e850828b0a4
Daniel Kliemsch <dk@lvl51.de>
2025-09-11 11:59:51 +0200

    patch: handle dates reset

--- client/SilverstripeDatepicker.vue
+++ client/SilverstripeDatepicker.vue
@@ -14,30 +14,34 @@ const props = defineProps<{
 const dates = ref(props.value ? JSON.parse(props.value) : []);
 
 const preparedValue = computed(() => {
-  const days = [...dates.value]
-    .map((item) => {
-      if (item) {
-        // is already an ISO date string (e.g. 2025-09-11)
-        if (typeof item === 'string' && item.length === 10) {
-          return item;
+  if (dates.value) {
+    const days = [...dates.value]
+      .map((item) => {
+        if (item) {
+          // is already an ISO date string (e.g. 2025-09-11)
+          if (typeof item === 'string' && item.length === 10) {
+            return item;
+          }
+
+          if (item instanceof Date) {
+            return DateTime.fromJSDate(item).toISODate()
+          }
         }
 
-        if (item instanceof Date) {
-          return DateTime.fromJSDate(item).toISODate()
-        }
-      }
+        return null;
+      })
+      .filter(Boolean)
+      .sort((a, b) => {
+        const aDate = DateTime.fromISO(a);
+        const bDate = DateTime.fromISO(b);
 
-      return null;
-    })
-    .filter(Boolean)
-    .sort((a, b) => {
-      const aDate = DateTime.fromISO(a);
-      const bDate = DateTime.fromISO(b);
+        return aDate - bDate;
+      });
 
-      return aDate - bDate;
-    });
+    return JSON.stringify(days);
+  }
 
-  return JSON.stringify(days);
+  return null;
 });
 </script>
 
